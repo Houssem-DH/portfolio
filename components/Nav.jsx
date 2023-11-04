44b578236b0013afc87947f42f190e065f2d7641
xdhoussem <houssemoodahel@gmail.com>
2023-11-04 17:31:15 +0100

    FIX

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,9 @@ import { useRouter } from "next/navigation";
 import axios from "axios";
 import toast from "react-hot-toast";
 
+import { connect } from "@/dbConfig/dbConfig"
+connect()
+
 function Nav() {
   const [isOpen, setIsOpen] = useState(false);
 
