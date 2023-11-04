dba67e26f909603873068eb5eb848aca57eae879
xdhoussem <houssemoodahel@gmail.com>
2023-11-04 17:12:20 +0100

    FIX

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -10,7 +10,7 @@ function Nav() {
   const [isOpen, setIsOpen] = useState(false);
 
   const router = useRouter();
-  const [userData, setUserData] = useState("");
+  const [userData, setUserData] = useState(null);
 
   useEffect(() => {
     getUserDetails();
