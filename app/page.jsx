d24d0de7213bdda41a2740837963a7285c78606f
xdhoussem <houssemoodahel@gmail.com>
2023-11-15 21:21:11 +0100

    FIX

--- app/page.jsx
+++ app/page.jsx
@@ -4,9 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main >
-<h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+
 
     </main>
   )
