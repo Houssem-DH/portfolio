8a0215a2a9a3166e831ab4427b6ecd93a3e6a427
xdhoussem <houssemoodahel@gmail.com>
2023-11-15 20:54:09 +0100

    LAB1

--- app/web3d/lab1/page.jsx
+++ app/web3d/lab1/page.jsx
@@ -1,24 +1,34 @@
 "use client";
 
-import React from 'react';
-import Head from 'next/head';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import { Model } from '@/components/Lab1Model';
+import React from "react";
+import Head from "next/head";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Model } from "@/components/Lab1Model";
 
 function Lab1() {
   return (
-    <div>
-      <Head>
-        <title>Your 3D Model Page</title>
-      </Head>
-      <h1>Your 3D Model Page</h1>
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 20, 10]} />
-        <Model /> {/* Use the Model component here */}
-        <OrbitControls />
-      </Canvas>
+    <div className="flex items-center justify-center h-screen">
+      
+      <div className="border-2 p-4">
+        
+        <Canvas
+          camera={{ position: [-10, 5, 7] }}
+          style={{ width: "1280px", height: "720px" }}
+        >
+          {/* Ambient light to provide some overall illumination */}
+          <ambientLight intensity={0.5} />
+          {/* Directional light simulating sunlight */}
+          <directionalLight position={[20, 10, 10]} intensity={1} />
+          <directionalLight position={[0, 0, 0]} intensity={1} />
+          <directionalLight position={[-10, -10, -10]} intensity={1} />
+          <directionalLight position={[-20, -20, -20]} intensity={1} />
+          {/* Point light for additional lighting, if needed */}
+          <pointLight position={[10, 10, 10]} />
+          <Model /> {/* Use the Model component here */}
+          <OrbitControls />
+        </Canvas>
+      </div>
     </div>
   );
 }
