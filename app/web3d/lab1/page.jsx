5b839ff3b94e740fd2757e4f6a7e436d6fe5f1b6
xdhoussem <houssemoodahel@gmail.com>
2023-11-15 18:15:09 +0100

    LAB1

--- app/web3d/lab1/page.jsx
+++ app/web3d/lab1/page.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import Head from 'next/head';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import { Model } from '@/components/ModelComponent';
+import { Model } from '@/components/Lab1Model';
 
 function Lab1() {
   return (
@@ -13,7 +13,7 @@ function Lab1() {
         <title>Your 3D Model Page</title>
       </Head>
       <h1>Your 3D Model Page</h1>
-      <Canvas style={{ width: '1920px', height: '1080px' }}>
+      <Canvas>
         <ambientLight />
         <pointLight position={[10, 20, 10]} />
         <Model /> {/* Use the Model component here */}
