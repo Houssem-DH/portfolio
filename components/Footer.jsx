8ac8bcd4440e00b27b4ffaac0441177e7e389661
xdhoussem <houssemoodahel@gmail.com>
2023-11-20 03:41:56 +0100

    FIX

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,25 +1,13 @@
 // Footer.js
 
 "use client";
-import React, { useEffect } from "react";
-import { useSpring, animated } from "react-spring";
-import { useInView } from "react-intersection-observer";
+import React from "react";
+
 
 const Footer = () => {
-  const [ref, inView] = useInView({
-    triggerOnce: true,
-  });
-
-  const style = useSpring({
-    opacity: inView ? 1 : 0,
-    transform: inView ? "translateY(0)" : "translateY(50px)",
-  });
+  
   return (
-    <animated.div
-      ref={ref}
-      style={style}
-      
-    >
+   
     <footer className="bg-slate-950/25 text-white py-8  z-50">
       
       <div className="container mx-auto text-center">
@@ -28,7 +16,7 @@ const Footer = () => {
       
       
     </footer>
-    </animated.div>
+   
   );
 };
 
