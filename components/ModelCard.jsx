7822c5892931f22600953e42251d2d0165abe44c
xdhoussem <houssemoodahel@gmail.com>
2023-11-20 03:20:44 +0100

    FIX

--- components/ModelCard.jsx
+++ components/ModelCard.jsx
@@ -11,7 +11,9 @@ const ModelCard = ({
 }) => {
   return (
     <div className="bg-slate-950/25 text-white p-6 rounded-lg shadow-md mb-8">
+        <Link href={livePreviewLink} target="_blank" rel="noopener noreferrer">
       <Image src={image} alt={title} width={1920} height={1080} className="w-full h-auto mb-4 rounded-md" />
+      </Link>
       <h3 className="text-2xl font-bold mb-2 text-indigo-500">{title}</h3>
       <p className="text-gray-400 mb-4">{description}</p>
       <div className="flex space-x-4">
