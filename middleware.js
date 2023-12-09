92024e7fabcebfe1a2bc65ea7336528b2acec890
houssem dahel <houssemoodahel@gmail.com>
2023-12-09 18:18:02 +0100

    FIX AUTH

--- middleware.js
+++ middleware.js
@@ -6,7 +6,7 @@ export function middleware(request) {
     const path = request.nextUrl.pathname
 
     const isPublicPath = path === '/login' || path === '/signup'
-    const token = request.cookies.get('next-auth.session-token')?.value || ''
+    const token = request.cookies.get('__Secure-next-auth.session-token')?.value || ''
 
     if (isPublicPath && token) {
         return NextResponse.redirect(new URL('/', request.nextUrl))
