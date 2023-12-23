7a5fe7d9e7ed05cd4f4fb1ce964273318ff563de
houssem dahel <houssemoodahel@gmail.com>
2023-12-23 19:32:29 +0100

    FIX TABLE

--- app/university/web3d/table/page.jsx
+++ app/university/web3d/table/page.jsx
@@ -8,22 +8,12 @@ import Loading from "@/components/Loading";
 
 
 function Lab1() {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    // Simulate loading time
-    const timeout = setTimeout(() => {
-      setLoading(false);
-    }, 5000);
-
-    return () => clearTimeout(timeout);
-  }, []); // Run only on mount
+ 
   return (
    
     
     <div className="flex items-center justify-center min-h-screen">
-    {loading ? (
-      <Loading size={50} color="rgb(99 102 241 / 0.4)"/>
-    ) : (
+    
       <div className="border border-indigo-500 rounded-lg overflow-hidden shadow-md bordered-div">
       
         
@@ -32,23 +22,15 @@ function Lab1() {
           style={{ width: "1024px", height: "576px" }}
           
         >
-          {/* Ambient light to provide some overall illumination */}
-          <ambientLight intensity={0.5} />
-          {/* Directional light simulating sunlight */}
-          <directionalLight position={[20, 10, 10]} intensity={1} />
-          <directionalLight position={[0, 0, 0]} intensity={1} />
-          <directionalLight position={[-10, -10, -10]} intensity={1} />
-          <directionalLight position={[-20, -20, -20]} intensity={1} />
-          {/* Point light for additional lighting, if needed */}
-          <pointLight position={[10, 10, 10]} />
+          
           
           <Model /> {/* Use the Model component here */}
           
-          <OrbitControls />
+          
         </Canvas>
         
       </div>
-       )}
+       
     
    
     </div>
--- components/Lab1Model.jsx
+++ components/Lab1Model.jsx
@@ -1,130 +1,158 @@
-import React from 'react'
-import { useGLTF } from '@react-three/drei'
+import React from "react";
+import { useGLTF, useTexture, OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
+import * as THREE from "three";
+import { Perf } from "r3f-perf";
 
 export function Model(props) {
-  const { nodes, materials } = useGLTF('/web3d/lab1/LAB1.glb')
+  const { nodes, materials } = useGLTF("/web3d/lab1/LAB1.gltf");
+
+  const tableTexture = useTexture(
+    "/web3d/lab1/Textures/brown-wooden-textured-flooring-background.jpg"
+  );
+
+  const { orbit, perf } = useControls({
+    orbit: true,
+    perf: false,
+  });
+
+  const { backgroundColor } = useControls("Background", {
+    backgroundColor: "#051467",
+  });
+
+  const { lightColor1, lightColor2, lightColor3, lightColor4 } = useControls(
+    "Lights",
+    {
+      lightColor1: "#FFFFFF",
+      lightColor2: "#FFFFFF",
+      lightColor3: "#FFFFFF",
+      lightColor4: "#FFFFFF",
+    }
+  );
+  const { tablePosition, tableScale, tableColor } = useControls("Table", {
+    tablePosition: {
+      value: [8.436, 2.266, -0.009],
+      min: -20,
+      max: 22,
+      step: 1,
+    },
+    tableScale: {
+      value: [5.218, 0.157, 2.606],
+      min: 1,
+      max: 10,
+      step: 0.1,
+    },
+    tableColor: "#FFFFFF",
+  });
+  const { chair1Position, chair1Color } = useControls("Chair1", {
+    chair1Position: {
+      value: [14.626, -0.68, 1.056],
+      min: 4.626,
+      max: 24.626,
+      step: 1,
+    },
+    chair1Color: "#C18100",
+  });
+
   return (
-    <group {...props} dispose={null}>
-      <mesh geometry={nodes.Cube002.geometry} material={materials['Material.005']} position={[8.431, 2.266, -0.009]} scale={[5.218, 0.157, 2.606]} />
-      <mesh geometry={nodes.Cube004.geometry} material={materials['Material.014']} position={[8.436, 0.724, -0.012]} rotation={[3.137, -0.728, 3.133]} scale={1.398} />
-      <mesh geometry={nodes.Cube001.geometry} material={materials['Material.015']} position={[8.436, 0.724, -0.012]} rotation={[3.138, 0.635, 3.138]} scale={1.398} />
-      <mesh geometry={nodes.Cube.geometry} material={materials['Material.002']} position={[8.751, 7.025, -1.038]} scale={[281.87, 7.72, 112.255]} />
-      <mesh geometry={nodes.Chair_Legs006.geometry} material={materials['Material.029']} position={[7.786, -0.68, -5.277]} rotation={[0, -0.005, 0.163]} scale={4.294}>
-        <mesh geometry={nodes.BackRest006.geometry} material={materials['Material.022']} position={[-0.143, 0.686, -0.169]} rotation={[1.209, -0.153, -0.058]} />
-        <group position={[-0.347, 0.509, -0.002]} rotation={[0.208, 0.513, -0.589]}>
-          <mesh geometry={nodes.Circle020.geometry} material={materials['Material.010']} />
-          <mesh geometry={nodes.Circle020_1.geometry} material={materials['screw hole']} />
-        </group>
-        <group position={[-0.306, 0.64, -0.15]} rotation={[0.983, -0.713, -0.77]}>
-          <mesh geometry={nodes.Circle021.geometry} material={materials['Material.012']} />
-          <mesh geometry={nodes.Circle021_1.geometry} material={materials['screw hole']} />
-        </group>
-        <group position={[-0.259, 0.768, -0.198]} rotation={[1.077, -0.789, -0.597]}>
-          <mesh geometry={nodes.Circle022.geometry} material={materials['Material.011']} />
-          <mesh geometry={nodes.Circle022_1.geometry} material={materials['screw hole']} />
-        </group>
-        <mesh geometry={nodes.Seat006.geometry} material={materials['Material.021']} position={[-0.175, 0.487, 0.055]} rotation={[0, 0, -0.163]} />
-        <mesh geometry={nodes.support007.geometry} material={nodes.support007.material} position={[-0.2, 0.341, -0.185]} rotation={[0.147, -0.024, -0.161]} />
-      </mesh>
-      <mesh geometry={nodes.Chair_Legs001.geometry} material={materials['Material.028']} position={[11.554, -0.68, -5.277]} rotation={[0, -0.005, 0.163]} scale={4.294}>
-        <mesh geometry={nodes.BackRest001.geometry} material={materials['Material.023']} position={[-0.143, 0.686, -0.169]} rotation={[1.209, -0.153, -0.058]} />
-        <group position={[-0.347, 0.509, -0.002]} rotation={[0.208, 0.513, -0.589]}>
-          <mesh geometry={nodes.Circle023.geometry} material={materials['Material.010']} />
-          <mesh geometry={nodes.Circle023_1.geometry} material={materials['screw hole']} />
-        </group>
-        <group position={[-0.306, 0.64, -0.15]} rotation={[0.983, -0.713, -0.77]}>
-          <mesh geometry={nodes.Circle024.geometry} material={materials['Material.012']} />
-          <mesh geometry={nodes.Circle024_1.geometry} material={materials['screw hole']} />
-        </group>
-        <group position={[-0.259, 0.768, -0.198]} rotation={[1.077, -0.789, -0.597]}>
-          <mesh geometry={nodes.Circle025.geometry} material={materials['Material.011']} />
-          <mesh geometry={nodes.Circle025_1.geometry} material={materials['screw hole']} />
-        </group>
-        <mesh geometry={nodes.Seat001.geometry} material={materials['Material.020']} position={[-0.175, 0.487, 0.055]} rotation={[0, 0, -0.163]} />
-        <mesh geometry={nodes.support001.geometry} material={nodes.support001.material} position={[-0.2, 0.341, -0.185]} rotation={[0.147, -0.024, -0.161]} />
-      </mesh>
-      <mesh geometry={nodes.Chair_Legs002.geometry} material={materials['Material.032']} position={[9.374, -0.68, 5.086]} rotation={[Math.PI, 0.014, -2.978]} scale={4.294}>
-        <mesh geometry={nodes.BackRest002.geometry} material={materials['Material.027']} position={[-0.143, 0.686, -0.169]} rotation={[1.209, -0.153, -0.058]} />
-        <group position={[-0.347, 0.509, -0.002]} rotation={[0.208, 0.513, -0.589]}>
-          <mesh geometry={nodes.Circle026.geometry} material={materials['Material.010']} />
-          <mesh geometry={nodes.Circle026_1.geometry} material={materials['screw hole']} />
-        </group>
-        <group position={[-0.306, 0.64, -0.15]} rotation={[0.983, -0.713, -0.77]}>
-          <mesh geometry={nodes.Circle027.geometry} material={materials['Material.012']} />
-          <mesh geometry={nodes.Circle027_1.geometry} material={materials['screw hole']} />
-        </group>
-        <group position={[-0.259, 0.768, -0.198]} rotation={[1.077, -0.789, -0.597]}>
-          <mesh geometry={nodes.Circle028.geometry} material={materials['Material.011']} />
-          <mesh geometry={nodes.Circle028_1.geometry} material={materials['screw hole']} />
-        </group>
-        <mesh geometry={nodes.Seat002.geometry} material={materials['Material.018']} position={[-0.175, 0.487, 0.055]} rotation={[0, 0, -0.163]} />
-        <mesh geometry={nodes.support002.geometry} material={nodes.support002.material} position={[-0.2, 0.341, -0.185]} rotation={[0.147, -0.024, -0.161]} />
-      </mesh>
-      <mesh geometry={nodes.Chair_Legs003.geometry} material={materials['Material.031']} position={[5.607, -0.68, 5.049]} rotation={[Math.PI, 0.014, -2.978]} scale={4.294}>
-        <mesh geometry={nodes.BackRest003.geometry} material={materials['Material.026']} position={[-0.143, 0.686, -0.169]} rotation={[1.209, -0.153, -0.058]} />
-        <group position={[-0.347, 0.509, -0.002]} rotation={[0.208, 0.513, -0.589]}>
-          <mesh geometry={nodes.Circle029.geometry} material={materials['Material.010']} />
-          <mesh geometry={nodes.Circle029_1.geometry} material={materials['screw hole']} />
-        </group>
-        <group position={[-0.306, 0.64, -0.15]} rotation={[0.983, -0.713, -0.77]}>
-          <mesh geometry={nodes.Circle030.geometry} material={materials['Material.012']} />
-          <mesh geometry={nodes.Circle030_1.geometry} material={materials['screw hole']} />
-        </group>
-        <group position={[-0.259, 0.768, -0.198]} rotation={[1.077, -0.789, -0.597]}>
-          <mesh geometry={nodes.Circle031.geometry} material={materials['Material.011']} />
-          <mesh geometry={nodes.Circle031_1.geometry} material={materials['screw hole']} />
-        </group>
-        <mesh geometry={nodes.Seat003.geometry} material={materials['Material.017']} position={[-0.175, 0.487, 0.055]} rotation={[0, 0, -0.163]} />
-        <mesh geometry={nodes.support003.geometry} material={nodes.support003.material} position={[-0.2, 0.341, -0.185]} rotation={[0.147, -0.024, -0.161]} />
-      </mesh>
-      <mesh geometry={nodes.Chair_Legs004.geometry} material={materials['Material.030']} position={[2.314, -0.68, -1.134]} rotation={[-Math.PI, 1.569, -2.978]} scale={4.294}>
-        <mesh geometry={nodes.BackRest004.geometry} material={materials['Material.025']} position={[-0.143, 0.686, -0.169]} rotation={[1.209, -0.153, -0.058]} />
-        <group position={[-0.347, 0.509, -0.002]} rotation={[0.208, 0.513, -0.589]}>
-          <mesh geometry={nodes.Circle032.geometry} material={materials['Material.010']} />
-          <mesh geometry={nodes.Circle032_1.geometry} material={materials['screw hole']} />
-        </group>
-        <group position={[-0.306, 0.64, -0.15]} rotation={[0.983, -0.713, -0.77]}>
-          <mesh geometry={nodes.Circle033.geometry} material={materials['Material.012']} />
-          <mesh geometry={nodes.Circle033_1.geometry} material={materials['screw hole']} />
-        </group>
-        <group position={[-0.259, 0.768, -0.198]} rotation={[1.077, -0.789, -0.597]}>
-          <mesh geometry={nodes.Circle034.geometry} material={materials['Material.011']} />
-          <mesh geometry={nodes.Circle034_1.geometry} material={materials['screw hole']} />
-        </group>
-        <mesh geometry={nodes.Seat004.geometry} material={materials['Material.016']} position={[-0.175, 0.487, 0.055]} rotation={[0, 0, -0.163]} />
-        <mesh geometry={nodes.support004.geometry} material={nodes.support004.material} position={[-0.2, 0.341, -0.185]} rotation={[0.147, -0.024, -0.161]} />
-      </mesh>
-      <mesh geometry={nodes.Chair_Legs005.geometry} material={materials['Material.033']} position={[14.626, -0.68, 1.056]} rotation={[Math.PI, -1.562, -2.978]} scale={4.294}>
-        <mesh geometry={nodes.BackRest005.geometry} material={materials['Material.024']} position={[-0.143, 0.686, -0.169]} rotation={[1.209, -0.153, -0.058]} />
-        <group position={[-0.347, 0.509, -0.002]} rotation={[0.208, 0.513, -0.589]}>
-          <mesh geometry={nodes.Circle035.geometry} material={materials['Material.010']} />
-          <mesh geometry={nodes.Circle035_1.geometry} material={materials['screw hole']} />
-        </group>
-        <group position={[-0.306, 0.64, -0.15]} rotation={[0.983, -0.713, -0.77]}>
-          <mesh geometry={nodes.Circle036.geometry} material={materials['Material.012']} />
-          <mesh geometry={nodes.Circle036_1.geometry} material={materials['screw hole']} />
-        </group>
-        <group position={[-0.259, 0.768, -0.198]} rotation={[1.077, -0.789, -0.597]}>
-          <mesh geometry={nodes.Circle037.geometry} material={materials['Material.011']} />
-          <mesh geometry={nodes.Circle037_1.geometry} material={materials['screw hole']} />
-        </group>
-        <mesh geometry={nodes.Seat005.geometry} material={materials['Material.019']} position={[-0.175, 0.487, 0.055]} rotation={[0, 0, -0.163]} />
-        <mesh geometry={nodes.support005.geometry} material={nodes.support005.material} position={[-0.2, 0.341, -0.185]} rotation={[0.147, -0.024, -0.161]} />
-      </mesh>
-    </group>
-  )
+    <>
+      {perf ? <Perf position="top-left" /> : null}
+
+      {/* Ambient light to provide some overall illumination */}
+      <ambientLight intensity={0.5} />
+      {/* Directional light simulating sunlight */}
+      <directionalLight
+        position={[20, 10, 10]}
+        intensity={2}
+        color={lightColor1}
+      />
+      <directionalLight
+        position={[0, 0, 0]}
+        intensity={1}
+        color={lightColor2}
+      />
+      <directionalLight
+        position={[-10, -10, -10]}
+        intensity={1}
+        color={lightColor3}
+      />
+      <directionalLight
+        position={[-20, -20, -20]}
+        intensity={1}
+        color={lightColor4}
+      />
+      {/* Point light for additional lighting, if needed */}
+      <pointLight position={[10, 10, 10]} />
+
+      <OrbitControls enabled={orbit} />
+
+      <group {...props} dispose={null}>
+        <mesh
+          geometry={nodes.Background.geometry}
+          material={materials["Material.002"]}
+          position={[8.751, 7.025, -1.038]}
+          scale={[281.87, 7.72, 112.255]}
+        >
+          <meshStandardMaterial
+            color={backgroundColor}
+            side={THREE.DoubleSide}
+          />
+        </mesh>
+        <mesh
+          geometry={nodes.Table.geometry}
+          material={materials["Wood Black UA"]}
+          position={tablePosition}
+          scale={tableScale}
+        >
+          <meshStandardMaterial map={tableTexture} color={tableColor} />
+        </mesh>
+        <mesh
+          geometry={nodes.Chair3.geometry}
+          material={materials.Wood}
+          position={[7.786, -0.68, -5.277]}
+          rotation={[0, -0.005, 0.163]}
+          scale={4.294}
+        />
+        <mesh
+          geometry={nodes.Chair2.geometry}
+          material={materials.Wood}
+          position={[11.554, -0.68, -5.277]}
+          rotation={[0, -0.005, 0.163]}
+          scale={4.294}
+        />
+        <mesh
+          geometry={nodes.Chair6.geometry}
+          material={materials.Wood}
+          position={[9.374, -0.68, 5.086]}
+          rotation={[Math.PI, 0.014, -2.978]}
+          scale={4.294}
+        />
+        <mesh
+          geometry={nodes.Chair5.geometry}
+          material={materials.Wood}
+          position={[5.607, -0.68, 5.049]}
+          rotation={[Math.PI, 0.014, -2.978]}
+          scale={4.294}
+        />
+        <mesh
+          geometry={nodes.Chair4.geometry}
+          material={materials.Wood}
+          position={[2.314, -0.68, -1.134]}
+          rotation={[-Math.PI, 1.569, -2.978]}
+          scale={4.294}
+        />
+        <mesh
+          geometry={nodes.Chair1.geometry}
+          material={materials.Wood}
+          position={chair1Position}
+          rotation={[Math.PI, -1.562, -2.978]}
+          scale={4.294}
+        >
+          <meshStandardMaterial color={chair1Color} />
+        </mesh>
+      </group>
+    </>
+  );
 }
 
-useGLTF.preload('/web3d/lab1/LAB1.glb')
-
-
-
-
-
-
-
-
-
-
-
-
+useGLTF.preload("/web3d/lab1/LAB1.gltf");
