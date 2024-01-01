cb12fb8036d9a13aae11005704ca2a43668d9e49
houssem dahel <houssemoodahel@gmail.com>
2024-01-02 00:03:23 +0100

    RIW PROJECT

--- app/university/page.jsx
+++ app/university/page.jsx
@@ -20,7 +20,7 @@ const University = () => {
         transform: inView ? "translateY(0)" : "translateY(400px)",
       });
 
-  const models = [
+  const web3d_models = [
     {
         title: 'Table',
         description: 'Blender / THREEjs /R3F / GLTF ',
@@ -52,6 +52,23 @@ const University = () => {
     
     // Add more projects as needed
   ];
+  const riw_models = [
+    {
+        title: 'Project',
+        description: 'Information Retrieval / Steam Dataset / Python / API ',
+        image: "/images/riw/demo.png", // Replace with the actual path to your image
+        
+        livePreviewLink: 'https://github.com/Houssem-DH/IR_Games_Project',
+      },
+   
+
+    
+      
+
+
+    
+    // Add more projects as needed
+  ];
 
   return (
     <animated.div
@@ -60,20 +77,35 @@ const University = () => {
     
   >
     
-    <section className=" text-white py-16 mt-16 min-h-screen">
+    <section className=" text-white py-16 mt-8 ">
       <div className="container mx-auto text-center">
         <h2 className="text-4xl font-bold mb-8 services-heading relative">
           WEB3D
           <span className="glow-line"></span>
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {models.map((model, index) => (
+          {web3d_models.map((model, index) => (
+            <ModeltCard key={index} {...model} />
+          ))}
+        </div>
+      </div>
+    </section>
+
+    <section className=" text-white py-4 min-h-screen">
+      <div className="container mx-auto text-center">
+        <h2 className="text-4xl font-bold mb-8 services-heading relative">
+          Information Retrieval
+          <span className="glow-line"></span>
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {riw_models.map((model, index) => (
             <ModeltCard key={index} {...model} />
           ))}
         </div>
       </div>
     </section>
     </animated.div>
+
   );
 };
 
